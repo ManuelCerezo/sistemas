a41487ddf33cb8624020fb3f01cb4cc3078ae729
Manuel Cerezo <73239400+ManuelCerezo@users.noreply.github.com>
2022-01-11 21:09:48 +0100

    Add files via upload

---
+++ Cloud/youtube/docker_youtube/login.js
@@ -0,0 +1,16 @@
+$(document).ready(main)
+function main(){
+    
+}
+function login(username,pasword){
+    alert("username: "+username)
+    alert("password: "+pasword)
+}
+function register(username,name,apellido,email,password,passhint){
+    alert(username)
+    alert(name)
+    alert(apellido)
+    alert(email)
+    alert(password)
+    alert(passhint)
+}
